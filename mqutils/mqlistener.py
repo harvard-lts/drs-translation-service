8b5e671255fe9f1f66201bf716c0ea7004ae2f8e
Valdeva Crema <valdeva_crema@harvard.edu>
2022-03-14 15:21:29 -0400

    Hdc 44 Implement basic STOMP connection to DRS Queue (#2)
    * MQ Utils
    
    * Updated tests and naming
    
    * Docker container version number update

---
+++ mqutils/mqlistener.py
@@ -0,0 +1,97 @@
+import datetime, json, os, time, traceback, stomp
+
+# Subscription id is unique to the subscription in this case there is only one subscription per connection
+_sub_id = 1
+_reconnect_attempts = 0
+_max_attempts = 1000
+
+def connect_and_subscribe(connection_params):
+    print("************************ MQUTILS MQLISTENER - CONNECT_AND_SUBSCRIBE *******************************")
+    global _reconnect_attempts
+    _reconnect_attempts = _reconnect_attempts + 1
+    if _reconnect_attempts <= _max_attempts:
+        # TODO: Retry timer with exponential backoff
+        time.sleep(1)
+        try:
+            connection_params.conn.set_ssl([(connection_params.host, connection_params.port)])
+            if not connection_params.conn.is_connected():
+                connection_params.conn.connect(connection_params.user, connection_params.password, wait=True)
+                print(f'connect_and_subscribe connecting {connection_params.queue} to with connection id 1 reconnect attempts: {_reconnect_attempts}', flush=True)
+            else:
+                print(f'connect_and_subscibe already connected {connection_params.queue} to with connection id 1 reconnect attempts {_reconnect_attempts}', flush=True)
+        except Exception as e:
+            print('Exception on disconnect. reconnecting...')
+            print(traceback.format_exc())
+            connect_and_subscribe(connection_params)
+        else:
+            connection_params.conn.subscribe(destination=connection_params.queue, id=1, ack='client-individual')
+            _reconnect_attempts = 0
+    else:
+        print('Maximum reconnect attempts reached for this connection. reconnect attempts: {}'.format(_reconnect_attempts), flush=True)
+
+
+class MqListener(stomp.ConnectionListener):
+    def __init__(self, connection_params):
+        self.connection_params = connection_params
+        self.message_data = None
+        print('MqListener init')
+
+    def on_error(self, frame):
+        print('received an error "%s"' % frame.body)
+
+    def on_message(self, frame):
+        print("************************ MQUTILS MQLISTENER - ON_MESSAGE *******************************")
+        headers, body = frame.headers, frame.body
+        print('received a message headers "%s"' % headers)
+        print('message body "%s"' % body)
+
+        message_id = headers.get('message-id')
+        self.message_data = json.loads(body)
+        
+
+        #TODO- Handle
+        print(' message_data {}'.format(self.message_data))
+        print(' message_id {}'.format(message_id))
+
+    def on_disconnected(self):
+        print('disconnected! reconnecting...')
+        connect_and_subscribe(self.connection_params)
+        
+    def get_connection(self):
+        return self.connection_params.conn
+    
+    def get_message_data(self):
+        return self.message_data
+
+class ConnectionParams:
+    def __init__(self, conn, queue, host, port, user, password):
+        self.conn = conn
+        self.queue = queue
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+         
+
+def initialize_drslistener():
+    mqlistener = get_mqlistener()
+    conn = mqlistener.get_connection()
+    conn.set_listener('', mqlistener)
+    connect_and_subscribe(mqlistener.connection_params)
+    # http_clients://github.com/jasonrbriggs/stomp.py/issues/206
+    while True:
+        time.sleep(2)
+        if not conn.is_connected():
+            print('Disconnected in loop, reconnecting')
+            connect_and_subscribe(mqlistener.connection_params)
+
+def get_mqlistener():
+    host = os.getenv('DRS_MQ_HOST')
+    port = os.getenv('DRS_MQ_PORT')
+    user = os.getenv('DRS_MQ_USER')
+    password = os.getenv('DRS_MQ_PASSWORD')
+    drs_queue = os.getenv('DRS_QUEUE_NAME')
+    conn = stomp.Connection([(host, port)], heartbeats=(40000, 40000), keepalive=True)
+    connection_params = ConnectionParams(conn, drs_queue, host, port, user, password)
+    mqlistener = MqListener(connection_params)
+    return mqlistener
---
+++ mqutils/tests/integration/test_mqlistener.py
@@ -0,0 +1,26 @@
+import sys, os, pytest, logging, stomp, time
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+import mqlistener as mqlistener
+
+logging.basicConfig(format='%(message)s')
+
+def test_drs_listener():
+    '''Tests to see if the listener picks up a topic from the queue'''
+    mqlistenerobject = mqlistener.get_mqlistener()
+    
+    conn = mqlistenerobject.get_connection()
+    conn.set_listener('', mqlistenerobject)
+    mqlistener.connect_and_subscribe(mqlistenerobject.connection_params)
+    
+    counter = 0
+    #Try for 30 seconds then fail
+    while mqlistenerobject.get_message_data() is None:
+        time.sleep(2)
+        counter = counter+2
+        if not conn.is_connected():
+            mqlistener.connect_and_subscribe(mqlistenerobject.connection_params)
+        if counter >= 30:
+            assert False, "Could not find anything on the queue after 30 seconds"
+        
+    assert mqlistenerobject.get_message_data() is not None
+    assert type(mqlistenerobject.get_message_data()) is dict
