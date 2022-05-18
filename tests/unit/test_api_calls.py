f395459b767ea0a2f8c7f3e345095de6beeb277a
vac765 <valdeva_crema@harvard.edu>
2022-05-18 15:24:49 -0400

    Removal of DTS_ENDPOINT variable

--- tests/unit/test_api_calls.py
+++ tests/unit/test_api_calls.py
@@ -4,17 +4,17 @@ sys.path.append('app')
 dts_endpoint = os.getenv("DTS_ENDPOINT")
 
 def test_valid_loadreport_call():
-    response = requests.get( dts_endpoint + "/loadreport?filename=abc", verify=False)
+    response = requests.get("https://localhost:8443/loadreport?filename=abc", verify=False)
     assert response.status_code == 200
     
 def test_invalid_loadreport_call():
-    response = requests.get(dts_endpoint + "/loadreport", verify=False)
+    response = requests.get("https://localhost:8443/loadreport", verify=False)
     assert response.status_code == 400
 
 def test_valid_batchfailed_call():
-    response = requests.get(dts_endpoint + "/failedBatch?batchName=abc", verify=False)
+    response = requests.get("https://localhost:8443/failedBatch?batchName=abc", verify=False)
     assert response.status_code == 200
     
 def test_invalid_batchfailed_call():
-    response = requests.get(dts_endpoint + "/failedBatch", verify=False)
+    response = requests.get("https://localhost:8443/failedBatch", verify=False)
     assert response.status_code == 400
