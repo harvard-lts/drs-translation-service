d9c59e3a21482b02ca60135a8ae93d9a2281f2da
Jessi Jassal <jessica_jassal@harvard.edu>
2022-06-23 10:39:49 -0400

    get batch dir substring as package id

--- app/load_report_service/load_report_service.py
+++ app/load_report_service/load_report_service.py
@@ -10,7 +10,7 @@ logging.basicConfig(filename=logfile, level=loglevel, format="%(asctime)s:%(leve
 load_report_dir = os.getenv("LOADREPORT_PATH")
 dropbox_dir = os.getenv("DROPBOX_PATH")
 
-def handle_load_report(package_id, load_report_name, dry_run = False):
+def handle_load_report(load_report_name, dry_run = False):
     #Strip off the LOADREPORT_ to get the batch name
     if (not load_report_name.startswith("LOADREPORT_")):
         raise LoadReportException("ERROR Expected load report name, {}, to begin with LOADREPORT_".format(load_report_name))
@@ -34,12 +34,16 @@ def handle_load_report(package_id, load_report_name, dry_run = False):
     
     nrs_prefix = os.getenv("NRS_PREFIX")    
     urn = os.path.join(nrs_prefix, obj_osn)
+    # remove trailing "-batch"
+    package_id = batch_name[0:-6]
     mqutils.notify_ingest_status_process_message(package_id, "success", urn)
     return urn
     
 
-def handle_failed_batch(package_id, batch_name, dry_run = False):
+def handle_failed_batch(batch_name, dry_run = False):
     #Send failed notification
+    # remove trailing "-batch"
+    package_id = batch_name[0:-6]
     mqutils.notify_ingest_status_process_message(package_id, "failed")
     #Delete batch from dropbox
     # TODO: Fix delete
