f7e4893fd3962371cbbb87daf1120c9f806ed14a
Guillermo Portas <hey@gportas.me>
2022-06-29 16:11:03 +0100

    DRS listener removal (#31)
    * Removed: unused DRS listener
    
    * Fixed: wrongly deleted ssl set code line recovered

--- app/mqresources/mqutils.py
+++ app/mqresources/mqutils.py
@@ -75,57 +75,7 @@ def notify_ingest_status_process_message(package_id, status, urn=None, queue=Non
         logging.debug(message)
     except Exception as e:
         logging.error(e)
-        raise (e)
-    return message
-
-
-def get_drs_mq_connection(queue=None):
-    logging.debug("************************ MQUTILS - GET_DRS_MQ_CONNECTION *******************************")
-    try:
-        host = os.getenv('DRS_MQ_HOST')
-        port = os.getenv('DRS_MQ_PORT')
-        user = os.getenv('DRS_MQ_USER')
-        password = os.getenv('DRS_MQ_PASSWORD')
-        if (queue is None):
-            drs_queue = os.getenv('DRS_QUEUE_CONSUME_NAME')
-        else:
-            drs_queue = queue
-        logging.debug("************************ QUEUE: {} *******************************".format(drs_queue))
-
-        conn = stomp.Connection([(host, port)], heartbeats=(40000, 40000), keepalive=True)
-        conn.set_ssl([(host, port)])
-        connection_params = ConnectionParams(conn, drs_queue, host, port, user, password, "auto")
-        conn.connect(user, password, wait=True)
-    except Exception as e:
-        logging.error(e)
-        raise (e)
-    return connection_params
-
-
-def notify_mock_drs_trigger_message(package_id):
-    '''Creates a mock message that indicates that the drs'''
-    logging.debug("************************ MQUTILS - CREATE_PROCESS_MESSAGE *******************************")
-    message = "No message"
-    try:
-        # Add more details that will be needed from the load report.
-        msg_json = {
-            "package_id": package_id,
-            "application_name": "Dataverse"
-        }
-        queue = os.getenv('DRS_QUEUE_PUBLISH_NAME')
-
-        expiration = _get_expiration()
-
-        logging.debug("msg json:")
-        logging.debug(msg_json)
-        message = json.dumps(msg_json)
-        connection_params = get_drs_mq_connection(queue)
-        connection_params.conn.send(queue, message, headers={"persistent": "true", "expires": expiration})
-        logging.debug("MESSAGE TO QUEUE create_initial_queue_message")
-        logging.debug(message)
-    except Exception as e:
-        logging.error(e)
-        raise (e)
+        raise e
     return message
 
 
