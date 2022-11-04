eeac974f9049618e27cf1784e41413270710d227
Jessi Jassal <jessica_jassal@harvard.edu>
2022-11-04 09:46:00 -0400

    Add properties files for opaque container

--- app/translation_service/translate_data_structure_service.py
+++ app/translation_service/translate_data_structure_service.py
@@ -1,22 +1,23 @@
 import os, os.path, logging, shutil, glob
 
-logfile=os.getenv('LOGFILE_PATH', 'drs_translation_service')
-loglevel=os.getenv('LOGLEVEL', 'WARNING')
+logfile = os.getenv('LOGFILE_PATH', 'drs_translation_service')
+loglevel = os.getenv('LOGLEVEL', 'WARNING')
 logging.basicConfig(filename=logfile, level=loglevel)
 
+
 def translate_data_structure(package_path, supplemental_deposit_data, depositing_application):
-    #Project name is the doi-name
-    #Batch name doi-name-batch
-    batch_name= os.path.basename(package_path) + "-batch"
+    # Project name is the doi-name
+    # Batch name doi-name-batch
+    batch_name = os.path.basename(package_path) + "-batch"
     batch_dir = os.path.join(package_path, batch_name)
-    #Object name is the doi-name
-    object_name= os.path.basename(package_path)
+    # Object name is the doi-name
+    object_name = os.path.basename(package_path)
     object_dir = os.path.join(batch_dir, object_name)
-    
+
     aux_object_dir = os.path.join(package_path, "_aux", batch_name, object_name)
     os.makedirs(aux_object_dir, exist_ok=True)
     os.makedirs(object_dir, exist_ok=True)
-    
+
     application_name = depositing_application
     if (application_name == "Dataverse"):
         is_extracted_package = os.getenv("EXTRACTED_PACKAGE_DVN", 'False').lower()
@@ -24,78 +25,82 @@ def translate_data_structure(package_path, supplemental_deposit_data, depositing
     elif (application_name == "ePADD"):
         content_model = os.getenv("EPADD_CONTENT_MODEL", "opaque container")
     else:
-        raise Exception("Unexpected application_name {}".format(application_name))    
-    
+        raise Exception("Unexpected application_name {}".format(application_name))
+
     if (content_model.lower() == "opaque"):
         __handle_opaque_directory_mapping(package_path, object_dir, aux_object_dir, is_extracted_package)
     elif (content_model.lower() == "opaque container"):
         __handle_opaque_container_directory_mapping(package_path, object_dir, aux_object_dir)
     else:
-        raise Exception("Content model {} is not yet supported".format(content_model))   
-                
+        raise Exception("Content model {} is not yet supported".format(content_model))
+
     return batch_dir
+
 
 def __handle_opaque_directory_mapping(package_path, object_dir, aux_object_dir, is_extracted_package):
     parent_directory_path = package_path
-    if (is_extracted_package == "true"):    
-        #Make batch dir and object dir
+    if (is_extracted_package == "true"):
+        # Make batch dir and object dir
         os.makedirs(object_dir, exist_ok=True)
-        #/package_path/extracted
+        # /package_path/extracted
         extracted_files_dir = os.path.join(package_path, "extracted")
-        #/package_path/extracted/unzippeddir
+        # /package_path/extracted/unzippeddir
         extracted_files = os.listdir(extracted_files_dir)
         if (len(extracted_files) != 1):
-            raise Exception("{} directory expected 1 item but found {}".format(extracted_files_dir, len(extracted_files)))    
-        
+            raise Exception(
+                "{} directory expected 1 item but found {}".format(extracted_files_dir, len(extracted_files)))
+
         parent_directory_path = os.path.join(extracted_files_dir, extracted_files[0])
-    
+
     hascontent = __handle_content_files(object_dir, parent_directory_path)
-    
-    __copy_project_conf(package_path)
-    __copy_object_xml_and_rename_object(aux_object_dir, hascontent)
-    
+
+    __copy_project_conf_opaque(package_path)
+    __copy_object_xml_and_rename_object(aux_object_dir, hascontent, False)
+
     __handle_documentation_files(object_dir, parent_directory_path)
 
 
 def __handle_opaque_container_directory_mapping(package_path, object_dir, aux_object_dir):
     hascontent = __handle_content_files(object_dir, package_path)
 
-    __copy_project_conf(package_path)
-    __copy_object_xml_and_rename_object(aux_object_dir, hascontent)
+    __copy_project_conf_opaque_container(package_path)
+    __copy_object_xml_and_rename_object(aux_object_dir, hascontent, True)
 
     __handle_documentation_files(object_dir, package_path)
+
 
 def __handle_content_files(object_dir, extracted_path):
     content_list_string = os.getenv("CONTENT_FILES_AND_DIRS", "")
     content_list = content_list_string.split(",")
-    
+
     hascontent = False
     for item in content_list:
         item = item.strip()
         item_path = os.path.join(extracted_path, item)
         content_path = os.path.join(object_dir, "content")
-            
-         #If there is a wildcard, then move all under that item
+
+        # If there is a wildcard, then move all under that item
         if ("*" in item):
             for file in glob.glob(r'{}'.format(item_path)):
-                shutil.copy2(file, os.path.join(content_path, file)) 
+                shutil.copy2(file, os.path.join(content_path, file))
         elif os.path.exists(item_path):
             logging.debug("Moving content for {}".format(item_path))
             if not os.path.exists(content_path):
                 os.mkdir(content_path)
-            #If it is a path to a file, move the file
-            if (os.path.isfile(item_path)): 
+            # If it is a path to a file, move the file
+            if (os.path.isfile(item_path)):
                 print("Moving {} to {}".format(item_path, os.path.join(content_path, os.path.basename(item_path))))
                 logging.debug("Moving {} to {}".format(item_path, os.path.basename(item_path)))
                 shutil.copy2(item_path, os.path.join(content_path, os.path.basename(item_path)))
-            #If it is a directory, use the recursive call
+            # If it is a directory, use the recursive call
             else:
                 __move_files(item_path, item_path, content_path)
             hascontent = hascontent or True
         else:
             hascontent = hascontent or False
-    
+
     return hascontent
+
 
 def __move_files(root_dir, source, dest_dir):
     '''This method actually copies the files from source to destination rather than
@@ -113,58 +118,64 @@ def __move_files(root_dir, source, dest_dir):
                 print("File {} to {}".format(os.path.join(root, filename), os.path.join(dest_dir, filename)))
                 shutil.copy2(os.path.join(root, filename), os.path.join(dest_dir, filename))
 
+
 def __handle_documentation_files(object_dir, extracted_path):
     documentation_list_string = os.getenv("DOCUMENTATION_FILES_AND_DIRS", "")
     documentation_list = documentation_list_string.split(",")
-    
+
     doc_path = os.path.join(object_dir, "documentation")
     for item in documentation_list:
         item = item.strip()
         item_path = os.path.join(extracted_path, item)
-        #If there is a wildcard, then move all under that item
+        # If there is a wildcard, then move all under that item
         if ("*" in item):
             print("Wildcard {} ".format(item_path))
             print(glob.glob('{}'.format(item_path)))
             for file in glob.glob('{}'.format(item_path)):
                 print("file: {}".format(file))
-                shutil.copy2(os.path.join(item_path,file), os.path.join(doc_path, file)) 
+                shutil.copy2(os.path.join(item_path, file), os.path.join(doc_path, file))
         elif os.path.exists(item_path):
             if not os.path.exists(doc_path):
                 os.mkdir(doc_path)
-                
-            #If it is a path to a file, move the file
-            if (os.path.isfile(item_path)): 
+
+            # If it is a path to a file, move the file
+            if (os.path.isfile(item_path)):
                 print("Moving {} to {}".format(item_path, os.path.join(doc_path, os.path.basename(item_path))))
                 logging.debug("Moving {} to {}".format(item_path, os.path.basename(item_path)))
                 shutil.copy2(item_path, os.path.join(doc_path, os.path.basename(item_path)))
-            #If it is a directory, use the recursive call
+            # If it is a directory, use the recursive call
             else:
                 __move_files(item_path, item_path, doc_path)
-    
 
-def __copy_project_conf(project_dir):
-    project_conf = os.getenv("PROJECT_CONF_TEMPLATE")
+
+def __copy_project_conf_opaque(project_dir):
+    project_conf = os.getenv("OPAQUE_PROJECT_CONF_TEMPLATE")
     shutil.copy2(project_conf, os.path.join(project_dir, "project.conf"))
 
-def __copy_object_xml_and_rename_object(aux_object_dir, hascontent):
-    
+
+def __copy_project_conf_opaque_container(project_dir):
+    project_conf = os.getenv("OPAQUE_CONTAINER_PROJECT_CONF_TEMPLATE")
+    shutil.copy2(project_conf, os.path.join(project_dir, "project.conf"))
+
+
+def __copy_object_xml_and_rename_object(aux_object_dir, hascontent, is_opaque_container):
     if not hascontent:
         object_xml_template = os.getenv("OBJECT_XML_DOC_ONLY_TEMPLATE")
+    elif is_opaque_container:
+        object_xml_template = os.getenv("OPAQUE_CONTAINER_OBJECT_XML_TEMPLATE")
     else:
-        object_xml_template = os.getenv("OBJECT_XML_TEMPLATE")
-        
+        object_xml_template = os.getenv("OPAQUE_OBJECT_XML_TEMPLATE")
+
     object_xml = os.path.join(aux_object_dir, "object.xml")
     object_name = os.path.basename(aux_object_dir)
-    
+
     # Read in the template file
     with open(object_xml_template, 'r') as file:
         filedata = file.read()
 
     # Replace the object name
     filedata = filedata.replace('OBJECT_NAME', object_name)
-    
+
     # Write the object.xml file out in the aux directory
     with open(object_xml, 'w') as file:
-      file.write(filedata)
-    
-   
+        file.write(filedata)
