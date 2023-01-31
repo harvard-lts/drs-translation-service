82ece19e1617dc31cc7b6c3eef993847d9246689
vac765 <valdeva_crema@harvard.edu>
2023-01-31 16:37:27 -0500

    removing array parsing call

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-import logging, traceback, json, re
+import logging, traceback, re
 import os, os.path
 from logging.handlers import RotatingFileHandler
 
@@ -96,9 +96,7 @@ def create_app():
         #Don't actually reprocess if it is a dryrun
         if ("dryrun" not in args):
             try: 
-                unprocessed_batches_list = json.loads(args['unprocessed_exports'])
-              
-                for batch_path in unprocessed_batches_list:
+                for batch_path in args['unprocessed_exports']:
                     logging.debug("Reprocessing {}".format(batch_path))
                     reprocess_batch(batch_path)
                 
