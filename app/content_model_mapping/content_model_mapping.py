80a516e37815d5b48f6bbb759fbf5caa96629215
vac765 <valdeva_crema@harvard.edu>
2023-08-14 10:31:59 -0400

    Removing translate

--- app/content_model_mapping/content_model_mapping.py
+++ app/content_model_mapping/content_model_mapping.py
@@ -18,19 +18,6 @@ class ContentModelMapping(ABC):
     def _handle_directory_mapping(self, package_path, object_dir, aux_object_dir):
         pass
         
-    def translate_data_structure(self, package_path):
-        batch_name = os.path.basename(package_path) + "-batch"
-        batch_dir = os.path.join(package_path, batch_name)
-        # Object name is the same as the package name
-        object_name = os.path.basename(package_path)
-        object_dir = os.path.join(batch_dir, object_name)
-    
-        aux_object_dir = os.path.join(package_path, "_aux", batch_name, object_name)
-        os.makedirs(aux_object_dir, exist_ok=True)
-        os.makedirs(object_dir, exist_ok=True)
-        self._handle_directory_mapping(package_path, object_dir, aux_object_dir)
-        
-        return batch_dir
                                 
     def _move_files(self, root_dir, source, dest_dir):
         '''This method actually copies the files from source to destination rather than
