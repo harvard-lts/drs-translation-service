e8d59f1608a0f158881db9ba3e31468ed097bacc
vac765 <valdeva_crema@harvard.edu>
2023-08-14 22:10:29 -0400

    pyflakes

--- app/content_model_mapping/opaque_container_content_model_mapping.py
+++ app/content_model_mapping/opaque_container_content_model_mapping.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import glob
 from content_model_mapping.content_model_mapping import ContentModelMapping
 from pathlib import Path
 
@@ -8,7 +9,6 @@ class OpqaueContainerContentModelMapping(ContentModelMapping):
     def handle_directory_mapping(self, package_path, object_dir, aux_object_dir):
         '''Moves the container into the container directory'''
         self.logger.debug("Formatting for opaque container content model")
-        hascontent = False
         container_dir = os.path.join(object_dir, "container")
         if not os.path.exists(container_dir):
             os.mkdir(container_dir)
@@ -21,21 +21,18 @@ class OpqaueContainerContentModelMapping(ContentModelMapping):
             filename = os.path.basename(file)
             if ".zip" in filename:
                 shutil.copy2(file, os.path.join(container_dir, filename))
-                hascontent = True
-    
+        
         for file in Path(package_path).glob('*.7z'):
             self.logger.debug("Found package: %s", file)
             filename = os.path.basename(file)
             if ".7z" in filename:
                 shutil.copy2(file, os.path.join(container_dir, filename))
-                hascontent = True
                 
         for file in Path(package_path).glob('*.gz'):
             self.logger.debug("Found package: %s", file)
             filename = os.path.basename(file)
             if ".gz" in filename:
                 shutil.copy2(file, os.path.join(container_dir, filename))
-                hascontent = True
                 
         project_conf = os.getenv("OPAQUE_CONTAINER_PROJECT_CONF_TEMPLATE")        
         self._copy_project_conf(package_path, project_conf)
