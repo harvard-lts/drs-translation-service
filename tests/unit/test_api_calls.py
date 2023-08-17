bd79bc8f278646327a9e8c43cabe33f9d1763c18
vac765 <valdeva_crema@harvard.edu>
2023-08-17 14:54:01 -0400

    load report service builder

--- tests/unit/test_api_calls.py
+++ tests/unit/test_api_calls.py
@@ -8,7 +8,7 @@ base_load_report_dir = os.getenv("BASE_LOADREPORT_PATH")
 
 def test_valid_loadreport_call():
     unit_test_helper.deposit_sample_load_report()
-    response = requests.get("https://localhost:8443/loadreport?filename={}&dryrun=True".format(unit_test_helper.sample_load_report), verify=False)
+    response = requests.get("https://localhost:8443/loadreport?filename={}&dropbox={}&dryrun=True".format(unit_test_helper.sample_load_report, "dvndev"), verify=False)
     assert response.status_code == 200
     unit_test_helper.cleanup_sample_load_report()
     
@@ -17,11 +17,11 @@ def test_invalid_loadreport_call():
     assert response.status_code == 400
     
 def test_invalid_loadreport_no_such_filename_call():
-    response = requests.get("https://localhost:8443/loadreport?filename=abc", verify=False)
+    response = requests.get("https://localhost:8443/loadreport?filename=abc&dropbox=dvndev", verify=False)
     assert response.status_code == 400
 
 def test_valid_batchfailed_call():
-    response = requests.get("https://localhost:8443/failedBatch?batchName=sample&dryrun=True", verify=False)
+    response = requests.get("https://localhost:8443/failedBatch?batchName=sample&dropbox=dvndev&dryrun=True", verify=False)
     assert response.status_code == 200
     
 def test_invalid_batchfailed_call():
@@ -29,7 +29,7 @@ def test_invalid_batchfailed_call():
     assert response.status_code == 400
 
 def test_invalid_batchfailed_no_such_batch_call():
-    response = requests.get("https://localhost:8443/failedBatch?batchName=abc", verify=False)
+    response = requests.get("https://localhost:8443/failedBatch?batchName=abc&dropbox=dvndev", verify=False)
     # This will work because we are not deleting the dir, when we attempt to delete the dir this will
     # need to be changed back to 400
     assert response.status_code == 200
