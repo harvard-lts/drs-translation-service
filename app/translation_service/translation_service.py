8549967ab08fe0576e41a4f40f7fd853f0aa2db9
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-09-01 10:41:40 -0400

    refactoring dts, in progress

--- app/translation_service/translation_service.py
+++ app/translation_service/translation_service.py
@@ -1,136 +1,106 @@
-import os, os.path, logging, shutil
+from abc import ABC, abstractmethod
+import os
+import os.path
+import logging
+import shutil
 
-from translate_data_structure.dataverse_translate_data_structure_service import DataverseTranslateDataStructureService
-from translate_data_structure.epadd_translate_data_structure_service import EpaddTranslateDataStructureService
-from batch_builder_service.dataverse_batch_builder_service import DataverseBatchBuilderService 
-from batch_builder_service.epadd_batch_builder_service import EpaddBatchBuilderService 
+class TranslationService(ABC):
+    '''Abstract class that defines how to translate the data for the 
+    inheriting classes.  Inheriting classes will perform
+    the actual translation'''
+    
+    def __init__(self):
+        self.logger = logging.getLogger('dts')
 
-logger = logging.getLogger('dts')
-base_load_report_dir = os.getenv("BASE_LOADREPORT_PATH")
-sample_load_report="/home/appuser/tests/data/sampleloadreport/LOADREPORT_sample.txt"
+    @abstractmethod
+    def get_admin_metadata(self, config_path):
+        pass
 
-def prepare_and_send_to_drs(package_dir, supplemental_deposit_data, depositing_application, testing = False):
-    #Set up directories
-    batch_name = os.path.basename(package_dir) + "-batch"
-    batch_dir = os.path.join(package_dir, batch_name)
-    
-    #This if-else is used for content model mapping for now.
-    if depositing_application == "Dataverse":
-        translate_service = DataverseTranslateDataStructureService()
-        batch_builder_service = DataverseBatchBuilderService()
-    else:
-        translate_service = EpaddTranslateDataStructureService()
-        batch_builder_service = EpaddBatchBuilderService()
-    
-    translate_service.translate_data_structure(package_dir)
-    #Run BB
-    batch_builder_service.process_batch(package_dir, batch_name, supplemental_deposit_data)
-    
-    #Move Batch to incoming
-    batch_dir = __move_batch_to_incoming(package_dir, batch_dir)
-    
-    if not testing:
-    #Remove old project dir
-        __cleanup_project_dir(package_dir)
-    
-    #Update batch_dir permissions
-    __update_permissions(batch_dir)
-    
-    #Add LOADING file to package directory if we are not testing
-    if not testing:
-        __create_loading_file(batch_dir)
-    #If testing, place a mock load report to allow for the flow to continue
-    else:
-        if ('dropbox_name' not in supplemental_deposit_data):
-            raise Exception("When testing, dropbox_name must be supplied in the supplemental deposit data")
-        __place_mock_load_report(os.path.basename(batch_dir), supplemental_deposit_data["dropbox_name"])
-    
-    return batch_dir
+    @abstractmethod
+    def _get_translate_data_structure_service():
+        pass
 
-def parse_drsconfig_metadata(drs_config_path):
-    admin_metadata = {}
-    try:
-        #This will throw an error if the file is missing which is handled in the try-except
-        with open(drs_config_path, 'r', encoding='UTF-8') as file:
-            metadata = file.read().splitlines()
-            metadata_dict = {}
-            for val in metadata:
-                if len(val) > 0:
-                    split_val = val.split('=')
-                    metadata_dict[split_val[0]] = split_val[1]
-            
-            try:
-                #This will throw an error if any key is missing and is handled in the try-except
-                admin_metadata = {        
-                    "accessFlag": metadata_dict["accessFlag"],
-                    "contentModel": metadata_dict["contentModel"],
-                    "depositingSystem": metadata_dict["depositingSystem"],
-                    "firstGenerationInDrs": metadata_dict["firstGenerationInDrs"],
-                    "objectRole": metadata_dict["objectRole"],
-                    "usageClass": metadata_dict["usageClass"],
-                    "storageClass": metadata_dict["storageClass"],
-                    "ownerCode": metadata_dict["ownerCode"],
-                    "billingCode": metadata_dict["billingCode"],
-                    "resourceNamePattern": metadata_dict["resourceNamePattern"],
-                    "urnAuthorityPath": metadata_dict["urnAuthorityPath"],
-                    "depositAgent": metadata_dict["depositAgent"],
-                    "depositAgentEmail": metadata_dict["depositAgentEmail"],
-                    "successEmail": metadata_dict["successEmail"],
-                    "failureEmail": metadata_dict["failureEmail"],
-                    "successMethod": metadata_dict["successMethod"],
-                    "adminCategory": metadata_dict.get("adminCategory"),
-                    "embargoBasis": metadata_dict.get("embargoBasis")
-                }
-            except KeyError as err:
-                logger.error("Missing a key in " + drs_config_path +" file: " + str(err))
+    def prepare_and_send_to_drs(self, package_dir, supplemental_deposit_data, depositing_application, testing = False):
+        #Set up directories
+        batch_name = os.path.basename(package_dir) + "-batch"
+        batch_dir = os.path.join(package_dir, batch_name)
+        
+        #This if-else is used for content model mapping for now.
+        '''
+        if depositing_application == "Dataverse":
+            translate_service = DataverseTranslateDataStructureService()
+            batch_builder_service = DataverseBatchBuilderService()
+        else:
+            translate_service = EpaddTranslateDataStructureService()
+            batch_builder_service = EpaddBatchBuilderService()
+        '''    
+        
+        translate_service.translate_data_structure(package_dir)
+        #Run BB
+        batch_builder_service.process_batch(package_dir, batch_name, supplemental_deposit_data)
+        
+        #Move Batch to incoming
+        batch_dir = __move_batch_to_incoming(package_dir, batch_dir)
+        
+        if not testing:
+        #Remove old project dir
+            self.__cleanup_project_dir(package_dir)
+        
+        #Update batch_dir permissions
+        self.__update_permissions(batch_dir)
+        
+        #Add LOADING file to package directory if we are not testing
+        if not testing:
+            self.__create_loading_file(batch_dir)
+        #If testing, place a mock load report to allow for the flow to continue
+        else:
+            if ('dropbox_name' not in supplemental_deposit_data):
+                raise Exception("When testing, dropbox_name must be supplied in the supplemental deposit data")
+            self.__place_mock_load_report(os.path.basename(batch_dir), supplemental_deposit_data["dropbox_name"])
+        
+        return batch_dir
 
-    except FileNotFoundError:
-        logger.error("drsConfig.txt does not exist for path: "+ drs_config_path)
-    
-    return admin_metadata
+    def __move_batch_to_incoming(project_dir, batch_dir):
+        #dropbox is the path above the project
+        dropbox_path = os.path.dirname(project_dir)
+        shutil.move(batch_dir, dropbox_path)
+        return os.path.join(dropbox_path, os.path.basename(batch_dir))   
 
-def __move_batch_to_incoming(project_dir, batch_dir):
-    #dropbox is the path above the project
-    dropbox_path = os.path.dirname(project_dir)
-    shutil.move(batch_dir, dropbox_path)
-    return os.path.join(dropbox_path, os.path.basename(batch_dir))   
+    def __cleanup_project_dir(self, project_dir):
+        '''Remove project directory'''
+        shutil.rmtree(project_dir) 
 
+    def __create_loading_file(self, batch_dir):
+        '''Creates a LOADING file for DRS'''
+        loading_file = os.path.join(batch_dir, "LOADING")
+        open(loading_file, 'a').close()
 
-def __cleanup_project_dir(project_dir):
-    '''Remove project directory'''
-    shutil.rmtree(project_dir) 
+    def __update_permissions(self, batch_dir):
+        '''
+        Sets group and r/w permission for DRS as follows:
+            Directory - batch_dir
+            User - hdc3aadm (55020)
+            Group - guestftp (4000)
+        '''
+        for root, dirs, files in os.walk(batch_dir):
+            for dir in dirs:
+                os.chown(os.path.join(root, dir), 55020, 4000)
+                os.chmod(os.path.join(root, dir), 0o775)
+            for file in files:
+                os.chown(os.path.join(root, file), 55020, 4000)
+                os.chmod(os.path.join(root, file), 0o775)
+        os.chown(batch_dir, 55020, 4000)
+        os.chmod(batch_dir, 0o775)
 
-def __create_loading_file(batch_dir):
-    '''Creates a LOADING file for DRS'''
-    loading_file = os.path.join(batch_dir, "LOADING")
-    open(loading_file, 'a').close()
-
-def __update_permissions(batch_dir):
-    '''
-    Sets group and r/w permission for DRS as follows:
-        Directory - batch_dir
-        User - hdc3aadm (55020)
-        Group - guestftp (4000)
-    '''
-    for root, dirs, files in os.walk(batch_dir):
-        for dir in dirs:
-            os.chown(os.path.join(root, dir), 55020, 4000)
-            os.chmod(os.path.join(root, dir), 0o775)
-        for file in files:
-            os.chown(os.path.join(root, file), 55020, 4000)
-            os.chmod(os.path.join(root, file), 0o775)
-    os.chown(batch_dir, 55020, 4000)
-    os.chmod(batch_dir, 0o775)
-    
-    
-def __place_mock_load_report(batch_name, dropbox_name):
-    #Make sure to append incoming if it is a real dropbox
-    if dropbox_name != "":
-        dropbox_name = os.path.join(dropbox_name, "incoming")
-    batch_load_report_dir = os.path.join(base_load_report_dir, dropbox_name, batch_name)
-    #Create dir in LR dir
-    os.mkdir(batch_load_report_dir)
-    
-    mock_load_report_name = "LOADREPORT_{}.txt".format(batch_name)
-    mock_load_report_dest = os.path.join(batch_load_report_dir, mock_load_report_name)
-    shutil.copy(sample_load_report, mock_load_report_dest)
+    def __place_mock_load_report(self.batch_name, dropbox_name):
+        #Make sure to append incoming if it is a real dropbox
+        if dropbox_name != "":
+            dropbox_name = os.path.join(dropbox_name, "incoming")
+        batch_load_report_dir = os.path.join(base_load_report_dir, dropbox_name, batch_name)
+        #Create dir in LR dir
+        os.mkdir(batch_load_report_dir)
+        
+        mock_load_report_name = "LOADREPORT_{}.txt".format(batch_name)
+        mock_load_report_dest = os.path.join(batch_load_report_dir, mock_load_report_name)
+        shutil.copy(sample_load_report, mock_load_report_dest)
+        
