52156681952a22c1ae88fa4f5359fa0d028dad61
Chip Goines <chip_goines@harvard.edu>
2023-09-08 10:13:56 -0400

    fix assert

--- tests/unit/test_notifier.py
+++ tests/unit/test_notifier.py
@@ -1,9 +1,9 @@
 import pytest, json, sys
+from celery.result import AsyncResult
 sys.path.append('app')
 import notifier.notifier as notifier 
 
 def test_notifier():
     '''Formats the directory and verifies that all files ended up where they should be'''
-    message = notifier.send_error_notification("Test Subject from DTS", "Test Body from DTS", "dts@hu.onmicrosoft.com")
-    json_message = json.loads(message)
-    assert type(json_message) is dict
+    result = notifier.send_error_notification("Test Subject from DTS", "Test Body from DTS", "dts@hu.onmicrosoft.com")
+    assert isinstance(result, AsyncResult)
