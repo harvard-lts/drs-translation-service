04029323090ffe1af97b35ce25a3d70b3d044d06
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-09-13 16:04:31 -0400

    adding existing code snippet for detecting content type

--- app/content_model_mapping/content_model_mapping_builder.py
+++ app/content_model_mapping/content_model_mapping_builder.py
@@ -3,20 +3,21 @@ from content_model_mapping.audio_content_model_mapping import AudioContentModelM
 from content_model_mapping.document_content_model_mapping import DocumentContentModelMapping
 from content_model_mapping.stillimage_content_model_mapping import StillImageContentModelMapping
 from content_model_mapping.text_content_model_mapping import TextContentModelMapping
-
+from subprocess import check_output
 
 class ContentModelMappingBuilder():
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.logger.debug("ContentModelMappingBuilder initialized")
 
-    def get_content_model_mapping(self, package_path):
+    def get_content_model_mapping(self, directory):
 
         content_model = None
         # get the content model from the package path
         # by inspecting mime type of the files
         # example code snippet:
         # https://github.huit.harvard.edu/LTS/etds/blob/ba9a05cbf2ed25ff9f50d28503b849edb9ceec8e/bin/etds2drs.py#L698
+        content_model = self.__get_content_model(directory, filename)
         
         if content_model == "text":
             return TextContentModelMapping()
@@ -28,3 +29,28 @@ class ContentModelMappingBuilder():
             return AudioContentModelMapping()
         else:
             raise Exception("Unknown content model: " + content_model)
+   
+    def __get_content_model(self, package_path):
+        '''Get the content model from the package path'''
+        mime_type = check_output(['file', '-b', '--mime-type', os.path.join(directory, filename)]).strip().decode()
+
+		try: 
+			return {
+				# this is not a complete list
+				'application/pdf': 'document',
+				'text/plain': 'text',
+				'text/xml': 'text',
+				'text/sgml': 'text',
+				'image/jpeg': 'image',
+				'image/jp2': 'image',
+				'image/gif': 'image',
+				'image/tiff': 'image',
+				'image/x-photo-cd': 'image',
+				'audio/x-aiff': 'audio',
+				'audio/x-wave': 'audio',
+				'audio/mpeg': 'audio',
+				'audio/mp4': 'audio',
+				'application/vnd.rn-realmedia': 'audio'
+				}[mime_type]
+		except:
+			return 'opaque'    
