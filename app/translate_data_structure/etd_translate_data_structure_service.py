579b43a03d555921ba9e1baa1ac5d1941e7e963c
Chip Goines <chip_goines@harvard.edu>
2023-09-18 15:28:47 -0400

    updated to use ETD cm

--- app/translate_data_structure/etd_translate_data_structure_service.py
+++ app/translate_data_structure/etd_translate_data_structure_service.py
@@ -21,8 +21,25 @@ class ETDTranslateDataStructureService(TranslateDataStructureService):
 
     def _handle_content_model_mapping(self, package_path, object_dir, aux_object_dir):
         '''Handle the content model mapping'''
-        for filename in glob.glob(os.path.join(package_path, '*.*')):
-            self.cmm_builder.get_content_model_mapping(package_path, filename)
 
+        for filename in glob.glob(os.path.join(package_path, '*.*')):
+            content_model = self.cmm_builder.get_content_model_mapping(package_path, filename)
+            if ((isinstance(content_model, DocumentContentModelMapping)) and (self.document_cmm is None)):
+                self.document_cmm = content_model
+                self.document_cmm.handle_directory_mapping(package_path, object_dir, aux_object_dir)
+            elif ((isinstance(content_model, StillImageContentModelMapping)) and (self.still_image_cmm is None)):
+                self.still_image_cmm = content_model
+                self.still_image_cmm.handle_directory_mapping(package_path, object_dir, aux_object_dir)
+            elif ((isinstance(content_model, AudioContentModelMapping)) and (self.audio_cmm is None)):
+                self.audio_cmm = content_model
+                self.audio_cmm.handle_directory_mapping(package_path, object_dir, aux_object_dir)
+            elif ((isinstance(content_model, TextContentModelMapping)) and (self.text_cmm is None)):
+                self.text_cmm = content_model
+                self.text_cmm.handle_directory_mapping(package_path, object_dir, aux_object_dir)
+            elif ((isinstance(content_model, ETDOpaqueContentModelMapping)) and (self.opaque_cmm is None)):
+                self.opaque_cmm = content_model
+                self.opaque_cmm.handle_directory_mapping(package_path, object_dir, aux_object_dir)
+            else: # all content models have been created
+                break
 
 
